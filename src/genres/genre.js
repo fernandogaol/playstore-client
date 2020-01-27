86bca22b92f7f260d4c59823b776bf969df5c628
Fernando Gaol <ffilho@riolindo.org>
2020-01-26 22:42:51 -0700

    first commit

---
+++ src/genres/genre.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Genre(props) {
+  return (
+    <div className="apps">
+      <h2>{props.App}</h2>
+      <div className="app_category">Category: {props.Category}</div>
+      <div className="app_rating">Rating: {props.Rating}</div>
+      <div className="app_genres">Genres: {props.Genres}</div>
+    </div>
+  );
+}
